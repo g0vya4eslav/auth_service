cd27bbfa2d0000c32136d80f775ae3d4f9e461a1
sll1nk <sll1nk@yandex.ru>
2024-12-16 16:16:00 +0300

    .

--- auth/main.go
+++ auth/main.go
@@ -77,8 +77,5 @@ func main() {
 
 	http.HandleFunc("/auth", authRequestHandler)
 	fmt.Println("HTTP сервер запущен на порту 9000 для auth_request")
-	err := http.ListenAndServe("0.0.0.0:9000", nil)
-	if err != nil {
-		log.Fatal("Ошибка при запуске HTTP-сервера: ", err)
-	}
+	log.Fatal(http.ListenAndServe("0.0.0.0:9000", nil))
 }
